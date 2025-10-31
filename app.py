320adf70e2195c30fccd9639db1173e6d06f45d6
huuly01092003 <huuly1357@gmail.com>
2025-10-31 08:58:47 +0700

    First commit - update Excel Sen Email web app

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'default_secret_key') 
+
 @app.route('/')
 def index():
     return render_template('index.html')
